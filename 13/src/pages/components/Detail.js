bc7930fa74280255fc21cef145a55db8479af747
lunamgoncalvesg <lunamgoncalvesguerreiro@gmail.com>
2025-09-19 23:18:48 -0300

    a ver si anda el 13 ahora

--- 13/src/pages/components/Detail.js
+++ 13/src/pages/components/Detail.js
@@ -2,12 +2,16 @@ import Form from "./Form";
 
 export default function Detail({ data }) {
     return (
-    <div>
-        <br></br>
-        <h4>{data.name}</h4>
-        <p>Altura: {data.height} cm</p>
-        <p>Nacimiento: {data.birth}</p>
-        <Form character={data} />
-    </div>
+        <>
+            {data && (
+                <div>
+                <br></br>
+                <h4>{data.name}</h4>
+                <p>Altura: {data.height} cm</p>
+                <p>Nacimiento: {data.birth}</p>
+                <Form character={data} />
+                </div>
+            )}
+        </>
     );
 }
